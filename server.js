46f2b9f980e5ad4ab48133ed9235d9f8953dbb8e
Junaid Farooq <junaid@evercam.io>
2018-07-26 13:13:05 +0500

    Started saving message through sockets and payload.

--- server.js
+++ server.js
@@ -11,6 +11,24 @@ var server = app.listen(3000);
 io = require("socket.io").listen(server);
 
 io.sockets.on("connection", function(socket){
+
+  socket.once("disconnect", function(){
+    connections.splice(connections.indexOf(socket), 1);
+    socket.disconnect();
+    console.log("Disconnected: %s sockets connected", connections.length);
+    io.emit("disconnect");
+  });
+
+  socket.on("messageAdded", function(payload){
+    console.log(payload);
+    var newMessage = {
+      timeStamp: payload.timeStamp,
+      text: payload.text
+    }
+
+    io.emit("messageAdded", newMessage);
+  });
+
   connections.push(socket);
   console.log("Connected: %s sockets connected", connections.length);
 });
