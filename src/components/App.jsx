8fcace2f6df0cfbc7ef1eb4dad72d112d505b064
Junaid Farooq <junaid@evercam.io>
2018-07-26 11:45:17 +0500

    Added basic component.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,2 +1,19 @@
-import React, {component} from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render(){
+    return(
+      <div>This is my App</div>
+    )
+  }
+}
+
+export default App
